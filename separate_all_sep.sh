13dd1e8d0d21ff7f7e89490a0daca5b814641b45
Laurence Finston <Laurence.Finston@gmx.de>
2021-05-27 11:11:45 +0200

    Added this (shellscript) file to the repository as a convenience.
    It's generated by running TeX on `all_no_sep.tex'.  The shellscript separates
    `all_sep.pdf' into individual pages and then combines them appropriately into
    individual pdf files.

---
+++ separate_all_sep.sh
@@ -0,0 +1,23 @@
+##! /bin/bash
+
+pdfseparate all_sep.pdf all_sep-%02d.pdf
+
+pdfunite all_sep-01.pdf all_sep-04.pdf toc_ls.pdf
+
+pdfunite all_sep-05.pdf all_sep-06.pdf toc_a_h_ls.pdf
+
+pdfunite all_sep-07.pdf all_sep-08.pdf toc_i_o_ls.pdf
+
+pdfunite all_sep-09.pdf all_sep-10.pdf toc_p_z_ls.pdf
+
+mv all-sep-11.pdf toc_npt.pdf
+
+pdfunite all_sep-12.pdf all_sep-19.pdf composers.pdf
+
+pdfunite all_sep-20.pdf all_sep-27.pdf lyricists.pdf
+
+mv all-sep-28.pdf scanned.pdf
+
+mv all-sep-29.pdf pblcdomn.pdf
+
+exit 0
