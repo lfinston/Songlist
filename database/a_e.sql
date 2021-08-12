11d07cd17c2b48a1f7fbe3eb8774e454b0a9b66c
Laurence Finston <Laurence.Finston@gmx.de>
2021-08-12 23:19:54 +0200

    Edited.

---
+++ database/a_e.sql
@@ -0,0 +1,285 @@
+update Songs set eps_filenames = "allgdsch1.eps; allgdsch2.eps; allgdsch3.eps;" where title = "All God's Children";
+
+select title, music, eps_filenames from Songs where title = "All God's Children";
+
+update Songs set eps_filenames = "alliddrm.eps;" where title = "All I Do Is Dream Of You";
+
+update Songs set eps_filenames = "alnagn01.eps; alnagn02.eps;" where title = "Alone Again (Naturally)";
+
+update Songs set eps_filenames = "anythin1.eps; anythin2.eps;" where title = "Anything You Can Do";
+
+update Songs set eps_filenames = "aprshw01.eps; aprshw02.eps; aprshw03.eps; aprshw04.eps;" where title = "April Showers";
+
+update Songs set eps_filenames = "babyfac1.eps; babyfac2.eps;" where title = "Baby Face";
+
+update Songs set eps_filenames = "justtim1.eps; justtim2.eps;" where title = "Just in Time";
+
+update Songs set eps_filenames = "prtyovr1.eps; prtyovr2.eps;" where title = "Party's Over, The";
+
+update Songs set eps_filenames = "bewitchd1.eps; bewitchd2.eps;" where title = "Bewitched";
+
+update Songs set eps_filenames = "blumoon1.eps; blumoon2.eps;" where title = "Blue Moon";
+
+update Songs set eps_filenames = "bodysoul1.eps; bodysoul2.eps;" where title = "Body and Soul";
+
+update Songs set eps_filenames = "btnpvrc1.eps; btnpvrc2.eps;" where title = "Button Up Your Overcoat";
+
+update Songs set eps_filenames = "bltslvm1.eps; bltslvm2.eps;" where title = "By the Light of the Silvery Moon";
+
+update Songs set eps_filenames = "cabaret1.eps; cabaret2.eps;" where title = "Cabaret";
+
+update Songs set eps_filenames = "callme1.eps; callme2.eps;" where title = "Call Me";
+
+update Songs set eps_filenames = "carolin1.eps; carolin2.eps; carolin3.eps;" where title = "Carolina in the Morning";
+
+update Songs set eps_filenames = "chicago1.eps; chicago2.eps;" where title = "Chicago (That Toddling Town)";
+
+update Songs set eps_filenames = "claire01.eps; claire02.eps;" where title = "Claire";
+
+update Songs set eps_filenames = "barkrl01.eps; barkrl02.eps; barkrl03.eps; barkrl04.eps;" where title 
+= "Barcarole (Belle nuit, {\\^o} nuit d'amour)";
+
+update Songs set eps_filenames = "oiseaux1.eps; oiseaux2.eps; oiseaux3.eps;" where title = "Oiseaux dans les charmille, Les";
+
+update Songs set eps_filenames = "crcvdo01.eps; crcvdo02.eps;" where title = "Corcovado";
+
+update Songs set eps_filenames = "cupcof01.eps; cupcof02.eps;" where title = "Cup of Coffee, a Sandwich and You, A";
+
+update Songs set eps_filenames = "heart1.eps; heart2.eps;" where title = "Heart (You've Gotta Have Heart)";
+
+update Songs set eps_filenames = "desafin1.eps; desafin2.eps; desafin3.eps;" where title = "Desafinado";
+
+update Songs set eps_filenames = "diamnds1.eps; diamnds2.eps;" where title = "Diamonds are a Girl's Best Friend";
+
+update Songs set eps_filenames = "lacidr01.eps; lacidr02.eps; lacidr03.eps; lacidr04.eps; lacidr05.eps;" where title = "L{\\`a} ci darem la mano";
+
+update Songs set eps_filenames = "estsolhr1.eps; estsolhr2.eps;" where title = "Este Seu Olhar";
+
+update Songs set eps_filenames = "evtcuprs1.eps; evtcuprs2.eps;" where title = "Everything's Coming Up Roses";
+
+update Songs set eps_filenames = "verynext1.eps; verynext2.eps; verynext3.eps;" where title = "(I'll Marry) The Very Next Man";
+
+update Songs set eps_filenames = "lttltnbx1.eps; lttltnbx2.eps;" where title = "Little Tin Box";
+
+update Songs set eps_filenames = "fivefoot.eps;" where title = "Five Foot Two, Eyes of Blue";
+
+update Songs set eps_filenames = "flymoon1.eps; flymoon2.eps;" where title = "Fly Me to the Moon (In Other Words)";
+
+
+update Songs set eps_filenames = "frtscst1.eps; frtscst2.eps;" where title = "42nd Street";
+
+
+update Songs set eps_filenames = "lbvrstn1.eps; lbvrstn2.eps; lbvrstn3.eps; lbvrstn4.eps;" where title = "Lob des hohen Verstandes";
+
+update Songs set eps_filenames = "liedlei1.eps; liedlei2.eps; liedlei3.eps;" where title = "Wer hat das Liedlein erdacht?";
+
+update Songs set eps_filenames = "drchwldr01.eps; drchwldr02.eps; drchwldr03.eps;" where title = "Durch die W@{a}lder, durch die Auen";
+
+update Songs set eps_filenames = "frimfrm1.eps; frimfrm2.eps;" where title = "Frim Fram Sauce, The";
+
+update Songs set eps_filenames = "garota1.eps; garota2.eps;" where title = "Gar{\\\^o}ta de Ipanema";
+
+update Songs set eps_filenames = "golddig1.eps; golddig2.eps;" where title = "Gold Diggers' Song, The (We're in the Money)";
+
+update Songs set eps_filenames = "womninlv1.eps; womninlv2.eps;" where title = "Woman in Love, A";
+
+update Songs set eps_filenames = "hrtsoul1.eps; hrtsoul2.eps;" where title = "Heart and Soul";
+
+update Songs set eps_filenames = "hlmabab1.eps; hlmabab2.eps;" where title = "Hello! Ma Baby";
+
+update Songs set eps_filenames = "hererain.eps;" where title = "Here's That Rainy Day";
+
+update Songs set eps_filenames = "howkeep1.eps; howkeep2.eps;" where title = "How 'Ya Gonna Keep 'Em Down on the Farm?";
+
+update Songs set eps_filenames = "idwtswnf1.eps; idwtswnf2.eps;" where title = "I Don't Want to Set the World on Fire";
+
+update Songs set eps_filenames = "lksofyou1.eps; lksofyou2.eps;" where title = "I Like the Likes of You";
+
+update Songs set eps_filenames = "iwonder01.eps; iwonder02.eps;" where title = "I Wonder Who's Kissing Her Now";
+
+update Songs set eps_filenames = "illfollw1.eps; illfollw2.eps;" where title = "I'll Follow My Secret Heart";
+
+update Songs set eps_filenames = "illrmbr1.eps; illrmbr2.eps; illrmbr3.eps;" where title = "I'll Remember April";
+
+update Songs set eps_filenames = "imfrvrbb.eps;" where title = "I'm Forever Blowing Bubbles";
+
+update Songs set eps_filenames = "gonnasi1.eps; gonnasi2.eps;" where title like("%I'm Gonna Sit Right Down%");
+
+update Songs set eps_filenames = "inmdfrlv1.eps; inmdfrlv2.eps; inmdfrlv3.eps;" where title = "I'm in the Mood for Love";
+
+update Songs set eps_filenames = "imwldah1.eps; imwldah2.eps;" where title = "I'm Just Wild About Harry";
+
+update Songs set eps_filenames = "imagin01.eps; imagin02.eps;" where title = "Imagination";
+
+update Songs set eps_filenames = "incertz1.eps; incertz2.eps;" where title = "Incerteza";
+
+update Songs set eps_filenames = "insenstz.eps;" where title = "Insensatez";
+
+update Songs set eps_filenames = "isntrmn1.eps; isntrmn2.eps;" where title = "Isn't It Romantic?";
+
+update Songs set eps_filenames = "ithadbey.eps;" where title = "It Had to be You";
+
+update Songs set eps_filenames = "itnvrmnd1.eps; itnvrmnd2.eps;" where title = "It Never Entered My Mind";
+
+update Songs set eps_filenames = "itsdelv1.eps; itsdelv2.eps;" where title = "It's De-Lovely";
+
+update Songs set eps_filenames = "ivgtflng1.eps; ivgtflng2.eps;" where title = "I've Got a Feeling I'm Falling";
+
+update Songs set eps_filenames = "jealsy01.eps; jealsy02.eps; jealsy03.eps; jealsy04.eps; jealsy05.eps;" where title = "Jalousie ``Tango Tzigane'' (Jealousy)";
+
+update Songs set eps_filenames = "jpnsnd01.eps; jpnsnd02.eps; jpnsnd03.eps; jpnsnd04.eps; jpnsnd05.eps;" where title = "Japanese Sandman, The";
+
+update Songs set eps_filenames = "jttrbwlz.eps;" where title = "Jitterbug Waltz, The";
+
+update Songs set eps_filenames = "listsong1.eps; listsong2.eps;" where title = "Listen to My Song (Johnny's Song)";
+
+update Songs set eps_filenames = "monamimf1.eps; monamimf2.eps; monamimf3.eps;" where title = "Mon Ami, My Friend";
+
+update Songs set eps_filenames = "jntsjmp1.eps; jntsjmp2.eps;" where title = "Joint is Jumpin', The";
+
+update Songs set eps_filenames = "jstonmc1.eps; jstonmc2.eps;" where title = "Just One More Chance";
+
+update Songs set eps_filenames = "kpygbtf1.eps; kpygbtf2.eps;" where title = "Keep Young and Beautiful";
+
+update Songs set eps_filenames = "kpnmsch1.eps; kpnmsch2.eps;" where title = "Keepin' Out of Mischief Now";
+
+update Songs set eps_filenames = "sptmbsn1.eps; sptmbsn2.eps;" where title = "September Song";
+
+update Songs set eps_filenames = "girlmmn1.eps; girlmmn2.eps;" where title = "Girl of the Moment";
+
+update Songs set eps_filenames = "ldysinlv1.eps; ldysinlv2.eps;" where title = "Lady's in Love With You, The";
+
+update Songs set eps_filenames = "lazyrivr1.eps; lazyrivr2.eps;" where title = "Lazy River";
+
+update Songs set eps_filenames = "lvltrsn1.eps; lvltrsn2.eps;" where title = "Love Letters in the Sand";
+
+update Songs set eps_filenames = "lvrcmbk1.eps; lvrcmbk2.eps;" where title = "Lover Come Back to Me";
+
+update Songs set eps_filenames = "lullbrd1.eps; lullbrd2.eps;" where title = "Lullaby of Broadway";
+
+update Songs set eps_filenames = "dummrt01.eps; dummrt02.eps; dummrt03.eps;" where title = "Lied vom dummen Reiter, Das";
+
+update Songs set eps_filenames = "lpnsch01.eps; lpnsch02.eps;" where title = "Lippen schweigen";
+
+update Songs set eps_filenames = "maxim01.eps; maxim02.eps; maxim03.eps;" where title = "Da geh ich zu Maxim";
+
+update Songs set eps_filenames = "mkwhoop1.eps; mkwhoop2.eps;" where title = "Makin' Whoopee!";
+
+update Songs set eps_filenames = "manhattan01.eps; manhattan02.eps;" where title = "Manhattan";
+
+update Songs set eps_filenames = "masque01.eps; masque02.eps;" where title = "Mas Que Nada";
+
+update Songs set eps_filenames = "memrsof1.eps; memrsof2.eps;" where title = "Memories of You";
+
+update Songs set eps_filenames = "mnltbay1.eps; mnltbay2.eps;" where title = "Moonlight Bay";
+
+update Songs set eps_filenames = "mscgsrnd.eps;" where title = "Music Goes 'Round and Around, The";
+
+update Songs set eps_filenames = "myheart01.eps; myheart02.eps;" where title = "My Heart Stood Still";
+
+update Songs set eps_filenames = "mymlby01.eps; mymlby02.eps;" where title = "My Melancholy Baby";
+
+update Songs set eps_filenames = "myoldfl1.eps; myoldfl2.eps;" where title = "My Old Flame";
+
+update Songs set eps_filenames = "nearnss1.eps; nearnss2.eps;" where title = "Nearness of You, The";
+
+update Songs set eps_filenames = "nicewrk1.eps; nicewrk2.eps;" where title = "Nice Work If You Can Get It";
+
+update Songs set eps_filenames = "nbdswt01.eps; nbdswt02.eps; nbdswt03.eps;" where title = "Nobody's Sweetheart";
+
+update Songs set eps_filenames = "fdglfd01.eps; fdglfd02.eps; fdglfd03.eps; fdglfd04.eps;" where title = "Food, Glorious Food";
+
+update Songs set eps_filenames = "whwllbuy.eps;" where title = "Who Will Buy?";
+
+update Songs set eps_filenames = "grndlphn.eps;" where title = "On Green Dolphin Street";
+
+update Songs set eps_filenames = "icancook1.eps; icancook2.eps; icancook3.eps; icancook4.eps;" where title = "I Can Cook Too";
+
+update Songs set eps_filenames = "felici01.eps; felici02.eps;" where title = "Felicidade, A";
+
+update Songs set eps_filenames = "onssamor1.eps; onssamor2.eps; onssamor3.eps;" where title = "O Nosso Amor (Carnival Samba)";
+
+update Songs set eps_filenames = "samborf1.eps; samborf2.eps;" where title = "Samba de Orfeu";
+
+update Songs set eps_filenames = "ovrrnb01.eps; ovrrnb02.eps;" where title = "Over the Rainbow";
+
+update Songs set eps_filenames = "prunacbz.eps;" where title = "Por Una Cabeza";
+
+update Songs set eps_filenames = "polow01.eps; polow02.eps; polow03.eps;" where title = "Polowetzer T@{a}nze (``Stranger in Paradise'')";
+
+update Songs set eps_filenames = "prtgrl01.eps; prtgrl02.eps; prtgrl03.eps; prtgrl04.eps;" where title = "Pretty A Girl is Like a Melody, A";
+
+update Songs set eps_filenames = "raindys1.eps; raindys2.eps;" where title = "Rainy Days and Mondays";
+
+update Songs set eps_filenames = "rckbybby.eps;" where title = "Rock-a-Bye Your Baby (With a Dixie Melody)";
+
+update Songs set eps_filenames = "smbunta1.eps; smbunta2.eps;" where title = "Samba de Uma Nota So";
+
+update Songs set eps_filenames = "stdsfiav1.eps; stdsfiav2.eps;" where title like("Se Todos Fossem%");
+
+update Songs set eps_filenames = "shfnttw1.eps; shfnttw2.eps;" where title = "She's Funny That Way";
+
+update Songs set eps_filenames = "shnhrvm1.eps; shnhrvm2.eps;" where title = "Shine On Harvest Moon";
+
+update Songs set eps_filenames = "smblvs01.eps; smblvs02.eps;" where title = "Somebody Loves Me";
+
+update Songs set eps_filenames = "snqmdjbs1.eps; snqmdjbs2.eps;" where title like("%que me dejabas%");
+
+update Songs set eps_filenames = "smenevng1.eps; smenevng2.eps;" where title = "Some Enchanted Evening";
+
+update Songs set eps_filenames = "whatkind.eps;" where title = "What Kind of Fool Am I?";
+
+update Songs set eps_filenames = "moonfac1.eps; moonfac2.eps;" where title = "Moon-Faced, Starry-Eyed";
+
+update Songs set eps_filenames = "whtgood1.eps; whtgood2.eps;" where title = "What Good Would the Moon Be?";
+
+update Songs set eps_filenames = "swngstr1.eps; swngstr2.eps;" where title = "Swinging On A Star";
+
+update Songs set eps_filenames = "teatwo01.eps; teatwo02.eps; teatwo03.eps; teatwo04.eps; teatwo05.eps;" where title = "Tea for Two";
+
+update Songs set eps_filenames = "temptat1.eps; temptat2.eps;" where title = "Temptation";
+
+update Songs set eps_filenames = "thycnt01.eps; thycnt02.eps;" where title = "They Can't Take That Away From Me";
+
+update Songs set eps_filenames = "thngswd1.eps; thngswd2.eps;" where title = "Things We Did Last Summer, The";
+
+update Songs set eps_filenames = "thouswell01.eps; thouswell02.eps;" where title = "Thou Swell";
+
+update Songs set eps_filenames = "tgrdnlc1.eps; tgrdnlc2.eps;" where title = "Ti Guarder{\\`e}ro Nel Cuore (More)";
+
+update Songs set eps_filenames = "tictic01.eps; tictic02.eps; tictic03.eps; tictic04.eps; tictic05.eps;" where title = "Tico Tico no Fuba";
+
+update Songs set eps_filenames = "tomyoblg1.eps; tomyoblg2.eps;" where title = "Tomo y Obligo";
+
+update Songs set eps_filenames = "tphtwtt1.eps; tphtwtt2.eps;" where title = "Top Hat, White Tie and Tails";
+
+update Songs set eps_filenames = "twosleep.eps;" where title = "Two Sleepy People";
+
+update Songs set eps_filenames = "vrytht01.eps; vrytht02.eps;" where title = "Very Thought of You, The";
+
+update Songs set eps_filenames = "volvr1.eps; volvr2.eps;" where title = "Volver";
+
+update Songs set eps_filenames = "wave01.eps; wave02.eps;" where title = "Wave";
+
+update Songs set eps_filenames = "weveonly.eps;" where title = "We've Only Just Begun";
+
+update Songs set eps_filenames = "whtwndf1.eps; whtwndf2.eps;" where title = "What a Wonderful World";
+
+update Songs set eps_filenames = "whenlov1.eps; whenlov2.eps;" where title = "When I Fall in Love";
+
+update Songs set eps_filenames = "whensml1.eps; whensml2.eps; whensml3.eps;" where title = "When You're Smiling";
+
+update Songs set eps_filenames = "onlvam01.eps; onlvam02.eps;" where title = "Once in Love with Amy";
+
+update Songs set eps_filenames = "whwsibrn1.eps; whwsibrn2.eps;" where title = "Why Was I Born?";
+
+update Songs set eps_filenames = "wlngnoch1.eps; wlngnoch2.eps;" where title = "Wie lange noch?";
+
+update Songs set eps_filenames = "ychtclub1.eps; ychtclub2.eps;" where title = "Yacht Club Swing";
+
+update Songs set eps_filenames = "youmdm01.eps; youmdm02.eps; youmdm03.eps; youmdm04.eps;" where title = "You Made Me Love You";
+
+update Songs set eps_filenames = "ytkadvn1.eps; ytkadvn2.eps; ytkadvn3.eps;" where title = "You Took Advantage of Me";
+
+update Songs set eps_filenames = "yrnbdytl1.eps; yrnbdytl2.eps;" where title = "You're Nobody 'Til Somebody Loves You";
+
