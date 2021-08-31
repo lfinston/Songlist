af7c77eff5a3beea2ec48e949786e286e96a864c
Laurence Finston <Laurence.Finston@gmx.de>
2021-08-31 18:20:04 +0200

    Added.

---
+++ database/teddy_wilson.sql
@@ -0,0 +1,631 @@
+https://www.jpc.de/jpcng/jazz/detail/-/art/Teddy-Wilson-1912-1986-Teddy-Wilson/hnum/7213579
+
+1
+Somebody loves me
+2
+Sweet and simple
+3
+Liza
+4
+Rosetta
+5
+I wished on the moon what a little moonlight can do
+6
+Miss Brown to you
+7
+A sunbonnet blue
+8
+What a night, what a moon, what a girl
+9
+I'm painting the town red
+10
+It's too hot for words
+11
+Sweet Lorraine
+12
+Every now and then
+13
+It never dawned on me
+14
+Liza
+15
+Rosetta
+16
+Twenty-four hours a day
+17
+Yankee doodle never went to town
+18
+Eny meeny miny mo
+19
+If you were mine
+20
+
+I found a dream
+21
+On treasure island
+22
+These "N" that "N" those
+23
+Sugar plum
+24
+You let me down
+25
+Spreading rhythm around
+26
+I feel like a feather in the breeze
+27
+Breaking in a pair of shoes
+28
+Life begins when you're in love
+29
+(If I had) rhythm
+30
+In my nursery rhymes
+31
+Christopher Columbus
+32
+My melancholy baby
+33
+All my life
+34
+Mary had a little lamb
+35
+Too good to be true
+36
+Warmin ' up
+37
+Blues in C sharp minor
+38
+It's like reaching the moon
+39
+These foolish things
+40
+Why do I lie to myself about you
+41
+I cried for you
+42
+Guess who
+43
+You came to my rescue
+44
+Here's love in your eyes
+45
+You turned the tables on me
+46
+Sing, baby, sing
+47
+Easy to love
+48
+With thee I swing
+49
+The way you look tonight
+50
+Who loves you
+51
+Pennies from heaven
+52
+That's life I
+53
+Guess
+54
+Sailin'
+55
+O can't give you anything but love
+56
+Right or wrong
+57
+Where the lazy river goes by
+58
+Tea for two
+59
+I'll see you in my dreams
+60
+He ain't got rhythm
+61
+This year's kisses
+62
+Why was I born
+63
+I must have that man
+64
+The mood that I'm in
+65
+You showed me the way
+66
+Sentimental and melancholy
+67
+My last affair
+68
+Carelessly
+69
+How could you
+70
+Moanin' low
+71
+Fine and dandy
+72
+There's a lull in my life
+73
+It's swell of you
+74
+How am I to know
+75
+I'm coming, Virginia
+76
+Sun showers
+77
+Yours and mine
+78
+I'll get by
+79
+Mean to me
+80
+Foolin' myself
+81
+Easy living
+82
+I'll never be the same
+83
+I've found a new baby
+84
+You're my desire
+85
+Remember me
+86
+The hour of parting
+87
+Coquette
+88
+Big apple
+89
+You can't stop me from dreamin'
+90
+If I had you
+91
+You brought a new kind of love to me
+92
+Ain't misbehavin'
+93
+Just a mood
+94
+Honeysuckle rose
+95
+Nice work if you can get it
+96
+Things are looking up
+97
+My man
+98
+Can't help lovin' dat man
+99
+Don't blame me
+100
+Between the devil and the deep blue sea
+101
+My first impression of you
+102
+When you're smiling
+103
+I can't believe that you're in love with me
+104
+If dreams come true
+105
+Moments like this u.v.a.
+
+Disk 2 von 10
+
+    1
+
+I found a dream
+2
+On treasure island
+3
+These 'n' that 'n' those
+4
+Sugar plum
+5
+You let me down
+6
+Spreadin' rhythm around
+7
+I Feel Like A Feather In The Breeze
+8
+Breaking in a pair of shoes
+9
+Life Begins When You're In Love
+10
+(If I had) rhythm in my nursery rhymes
+11
+Christopher Columbus
+12
+My melancholy baby
+13
+All my life
+14
+Mary had a little lamb
+15
+Too good to be true
+16
+Warmin' up
+17
+Blues in C sharp minor
+18
+It's like reaching the moon
+19
+These foolish things (remind me of you)
+20
+
+    Why do I lie to myself about you?
+
+Disk 3 von 10
+
+    1
+
+I cried for you
+2
+Guess who
+3
+You come to my rescue
+4
+Here's love in your eyes
+5
+You turned the tables on me
+6
+Sing, baby, sing
+7
+Easy to love
+8
+With thee I swing
+9
+The way you look tonight
+10
+Who loves you?
+11
+Pennies from heaven
+12
+That's life I guess
+13
+Sailin'
+14
+I can't give you anything but love
+15
+Right or wrong (I'm with you)
+16
+Where the lazy river goes by
+17
+Tea for two
+18
+I'll see you in my dreams
+19
+He ain't got rhythm
+20
+
+    This year's kisses
+
+Disk 4 von 10
+
+    1
+
+Why was I born
+2
+I must have that man
+3
+The mood that I'm in
+4
+You showed me the way
+5
+Sentimental and melancholy
+6
+My last affair
+7
+Carelessly
+8
+How could you?
+9
+Moanin' low
+10
+Fine and dandy
+11
+There's a lull in my life
+12
+It's swell of you
+13
+How am I to know
+14
+I'm coming, Virginia
+15
+Sun showers
+16
+Yours & mine
+17
+I'll get by
+18
+Mean to me
+19
+Foolin' myself
+20
+
+    Easy living
+
+Disk 5 von 10
+
+    1
+
+I'll never be the same
+2
+I've found a new baby
+3
+You're my Desire
+4
+Remember me
+5
+The hour of parting
+6
+Coquette
+7
+Big apple
+8
+You can't stop me from dreamin'
+9
+If I had you
+10
+You brought a new kind of love to me
+11
+Ain't misbehavin'
+12
+Just a mood
+13
+Honeysuckle rose
+14
+Nice work if you can get it
+15
+Things are looking up
+16
+My man
+17
+Can't Help Lovin' That Man
+18
+Don't blame me
+19
+Between The Devil And The Deep Blue Sea
+20
+
+    My first impression of you
+
+Disk 6 von 10
+
+    1
+
+When you're smiling
+2
+I can't believe that you're in love with me
+3
+If dreams come true
+4
+Moments like this
+5
+I can't face the music (without singin' the blues)
+6
+Don't be that way
+7
+If I were you
+8
+You go to my head
+9
+I'll dream tonight
+10
+Jungle love
+11
+That old feeling
+12
+My blue heaven
+13
+Now it can be told
+14
+Laugh and call it love
+15
+On the bumpy road to love
+16
+A-tisket, a-tasket
+17
+Loch lomond
+18
+Tiger rag
+19
+I'll see you in my dreams
+20
+
+    Alice blue gown
+
+Disk 7 von 10
+
+    1
+
+Everybody's laughing
+2
+Here it is tomorrow again
+3
+Say it with a kiss
+4
+April in my heart
+5
+I'll never fail you
+6
+They say
+7
+You're so desirable
+8
+You're gonna see a lot of me
+9
+Hello, my darling
+10
+Let's dream in the moonlight
+11
+Coquette
+12
+China boy
+13
+Melody in F
+14
+When you and I were young, Maggie
+15
+What Shall I Say
+16
+It's easy to blame the weather
+17
+More than you know
+18
+Sugar
+19
+Why begin again?
+20
+
+    Jumpin' for Joy
+
+Disk 8 von 10
+
+    1
+
+Booly-ja-ja
+2
+The man I love
+3
+Exactly like you
+4
+Love grows on the white oak tree
+5
+This is the moment
+6
+Early session hop
+7
+Lady of mystery
+8
+Jumpin' On The Black And Whites
+9
+Little things that mean so much
+10
+Hallelujah
+11
+Some other spring
+12
+Wham (re bop boom bam)
+13
+Sweet Lorraine
+14
+Moon ray
+15
+Liza
+16
+Crying my soul out for you
+17
+In the mood
+18
+Cocoanut grove
+19
+71
+20
+
+    I never knew
+
+Disk 9 von 10
+
+    1
+
+Embraceable you
+2
+But not for me
+3
+Oh, Lady Be Good
+4
+Smoke gets in your eyes
+5
+Rosetta
+6
+I know that you know
+7
+Them there eyes
+8
+China boy
+9
+Body and soul
+10
+I can't get started
+11
+Out of nowhere
+12
+Prisoner of love
+13
+Every time we say Goodbye
+14
+Just you, just me
+15
+Just for the blues
+16
+This heart of mine
+17
+Bugle call rag
+18
+Running wild
+19
+I surrender dear
+20
+Memories of you
+21
+If dreams come true
+22
+
+    I can't get started
+
+Disk 10 von 10
+
+    1
+
+Stompin' at the savoy
+2
+Blues too
+3
+Cheek to cheek
+4
+Sunny morning
+5
+Why shouldn't I
+6
+Strange interlude
+7
+All of me
+8
+Hallelujah
+9
+You're my favourite memory
+10
+Long ago and far away
+11
+Penthouse serenade (when you're alone)
+12
+Don't worry 'bout me
+13
+I want to be happy
+14
+Just one of those things
+15
+Fine and dandy
+16
+I've got the world on a string
+17
+Ain't misbehavin'
+18
+You took advantage of me
+19
+Living in dreams
+20
+I'm yours
+21
+Time after time
+22
+Moon face, starry eyed
+23
+September song
+24
+
+    Moonlight on the ganges
+
