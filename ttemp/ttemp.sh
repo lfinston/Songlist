09a3e026b31be838205b068c3f92a93ecd1ba988
Laurence Finston <Laurence.Finston@gmx.de>
2022-12-31 06:18:23 +0100

    Added.

---
+++ ttemp/ttemp.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+#### ttemp.sh
+#### Created by Laurence D. Finston (LDF) Sat Dec 31 06:18:14 AM CET 2022
+
+aintsswt1.pdf
+aintsswt2.pdf
+mhrtstst1.pdf
+mhrtstst2.pdf
+thngswdd1.pdf
+thngswdd2.pdf
+
